464969b25596815f92334ccbdecff877bbd49381
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-28 01:53:11 -0800

    adreno_tz: Fix GPU target frequency calculation for high refresh rates
    It seems that the black-box GPU frequency selection algorithm is unaware
    of high refresh rates, and assumes a refresh rate of 60 Hz for its
    target frequencies. This results in frequencies that are too low being
    selected when a higher refresh rate, such as 90 Hz, is used.
    
    To remedy this, multiply the GPU's busy time by a ratio of the current
    refresh rate to 60 Hz. This makes msm-adreno-tz select the right
    frequencies needed to avoid stuttering.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -51,6 +51,12 @@ static const struct of_device_id dsi_display_dt_match[] = {
 	{}
 };
 
+#ifdef CONFIG_TARGET_PROJECT_K7T
+struct dsi_display *primary_display;
+#endif
+
+static unsigned int cur_refresh_rate = 60;
+
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -8034,6 +8040,11 @@ int dsi_display_pre_commit(void *display,
 	return rc;
 }
 
+unsigned int dsi_panel_get_refresh_rate(void)
+{
+	return READ_ONCE(cur_refresh_rate);
+}
+
 int dsi_display_enable(struct dsi_display *display)
 {
 	int rc = 0;
@@ -8073,6 +8084,7 @@ int dsi_display_enable(struct dsi_display *display)
 	mutex_lock(&display->display_lock);
 
 	mode = display->panel->cur_mode;
+	WRITE_ONCE(cur_refresh_rate, mode->timing.refresh_rate);
 
 	if (mode->dsi_mode_flags & DSI_MODE_FLAG_DMS) {
 		rc = dsi_panel_post_switch(display->panel);
